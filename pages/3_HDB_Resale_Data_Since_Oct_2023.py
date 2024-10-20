5b443b1d05184a76a1cf3d141868e508730e4e0d
darewinnie <cjy.winnie@gmail.com>
2024-10-20 16:34:44 +0800

    rev

--- pages/3_HDB_Resale_Data_Since_Oct_2023.py
+++ pages/3_HDB_Resale_Data_Since_Oct_2023.py
@@ -5,5 +5,5 @@ import streamlit as st
 df = pd.read_csv('./data/resaleoct23.csv')
 
 #Print CSV table
-st.title("HDB Resale Data SInce Oct 2023")  # add a title
+st.title("HDB Resale Data Since Oct 2023")  # add a title
 st.write(df)
