82612ba3550ed68f1df96ef9f21bf43a691b6ca0
darewinnie <cjy.winnie@gmail.com>
2024-10-24 20:50:50 +0800

    Update customer_query_handler_copy.py

--- logics/customer_query_handler_copy.py
+++ logics/customer_query_handler_copy.py
@@ -17,9 +17,8 @@ def hdb_json(user_message):
 
     system_message = f"""
     You will be provided with customer service queries about hdb town and resale price . \
-    Please answer based on the resale json file. \
+    Please answer based on the JSON file. \
     Please remember you have data for October 2023, November 2023, December 2023, January 2024, February 2024, March 2024, April 2024, May 2024, June 2024, July 2024, August 2024, September 2024 and October 2024 as "month": "2024-10" refer to October 2024 \
-    If you don't know, please say " I don't know, you may google for it" \
     Your response must start with Ans: \
     
     """
