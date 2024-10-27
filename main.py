e2582eaa391553c888de1b2de8ae64cc7dd695a8
darewinnie <cjy.winnie@gmail.com>
2024-10-27 21:25:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ st.title("HDB Resale Price Finder")
 form = st.form(key="form")
 form.subheader("Please ask me about HDB resale data")
 
-user_prompt = form.text_area("Eg. what is the average price of 4 room in Ang Mo Kio town", height=200)
+user_prompt = form.text_area("Eg. What is the average price of 4 room in Ang Mo Kio town", height=200)
 
 if form.form_submit_button("Submit"):
     
