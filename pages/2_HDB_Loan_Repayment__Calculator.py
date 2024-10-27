a52079a3f78f97de641d4e069196d9d81c6ca39f
darewinnie <cjy.winnie@gmail.com>
2024-10-27 16:21:29 +0800

    rev

--- pages/2_HDB_Loan_Repayment__Calculator.py
+++ pages/2_HDB_Loan_Repayment__Calculator.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-from helper_functions.utility import check_password  
-
-# Check if the password is correct.  
-if not check_password():  
-    st.stop()
 
 st.title("HDB Loan Repayment Calculator 💰")
 
