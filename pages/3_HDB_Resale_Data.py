8958972d58795d2853173c9955ca4cd53294a3d7
darewinnie <cjy.winnie@gmail.com>
2024-10-27 08:48:17 +0800

    Update 3_HDB_Resale_Data.py

--- pages/3_HDB_Resale_Data.py
+++ pages/3_HDB_Resale_Data.py
@@ -20,8 +20,8 @@ flat_types = df['flat_type'].unique()  # Adjust 'flat_type' to your column name
 selected_flat_type = st.sidebar.selectbox("Select Flat Type", options=["All"] + list(flat_types))
 
 # Filter by price range
-min_price = int(df['price'].min())  # Adjust 'price' to your column name
-max_price = int(df['price'].max())
+min_price = int(df['resale_price'].min())  # Adjust 'price' to your column name
+max_price = int(df['resale_price'].max())
 selected_price_range = st.sidebar.slider("Select Price Range", min_value=min_price, max_value=max_price, value=(min_price, max_price))
 
 # Apply filters
@@ -33,7 +33,7 @@ if selected_town != "All":
 if selected_flat_type != "All":
         filtered_df = filtered_df[filtered_df['flat_type'] == selected_flat_type]
 
-filtered_df = filtered_df[(filtered_df['price'] >= selected_price_range[0]) & (filtered_df['price'] <= selected_price_range[1])]
+filtered_df = filtered_df[(filtered_df['resale_price'] >= selected_price_range[0]) & (filtered_df['resale_price'] <= selected_price_range[1])]
 
 # Display the filtered DataFrame
 st.subheader("Filtered Data")
