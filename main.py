4cd3c25d6f51a303376d9cfed541400857dff23b
darewinnie <cjy.winnie@gmail.com>
2024-10-27 15:43:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if form.form_submit_button("Submit"):
 
     st.divider()
 
-    hdb_response = hdb_json_2(user_prompt)
+    hdb_response = hdb_json(user_prompt)
     st.write(hdb_response)
 
 with st.expander("Disclaimer"):
