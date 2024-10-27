b9f5d4e415bda60a4bc67e97c6e6e333d70a27ca
darewinnie <cjy.winnie@gmail.com>
2024-10-27 08:31:22 +0800

    Update 5_About_Us.py

--- pages/5_About_Us.py
+++ pages/5_About_Us.py
@@ -13,3 +13,7 @@ st.header("Project Scope")
 
 st.write("The application is for users to check out on past HDB resale data based on different categories (Eg. unit type, location, transaction year etc) to facilitate users to look for suitable HDB flat based on their budget.")
 
+st.header("Objective")
+
+st.write("1. Help user look for past hdb resale data easily using chatbot")
+st.write("2. Help user to estimate budget requirement such as downpayment and monthly installment using calculator")
