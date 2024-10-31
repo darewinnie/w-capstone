ab4139fd5d4115f0866010a08b273a17398be6d2
darewinnie <cjy.winnie@gmail.com>
2024-10-31 11:52:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import pandas as pd
 # from helper_functions import llm
-from logics.customer_query_handler_copy import hdb_json
-#from logics.hdb_json_query import hdb_json_2
+#from logics.customer_query_handler_copy import hdb_json
+from logics.hdb_query import hdb_json_2
 from helper_functions.utility import check_password  
 
 # Check if the password is correct.  
@@ -30,7 +30,7 @@ if form.form_submit_button("Submit"):
 
     st.divider()
 
-    hdb_response = hdb_json(user_prompt)
+    hdb_response = hdb_json_2(user_prompt)
     st.write(hdb_response)
 
 with st.expander("Disclaimer"):
