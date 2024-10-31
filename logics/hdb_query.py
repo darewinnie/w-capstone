f4e15c30ec47845bdfe073f4fe866252fd38c1b1
darewinnie <cjy.winnie@gmail.com>
2024-10-31 15:07:38 +0800

    Update hdb_query.py

--- logics/hdb_query.py
+++ logics/hdb_query.py
@@ -1,6 +1,7 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import chromadb
 from langchain_community.document_loaders import CSVLoader
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import Chroma
