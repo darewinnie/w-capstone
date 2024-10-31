daf222a455c7010c6daa4eaf0cd24ace1c44521b
darewinnie <cjy.winnie@gmail.com>
2024-10-31 15:03:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Set up and run this Streamlit App
+import('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import pandas as pd
 # from helper_functions import llm
