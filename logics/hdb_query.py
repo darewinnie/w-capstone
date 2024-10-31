cfbea6d870661d4d7da2f156ef01bbd4e438a9ea
darewinnie <cjy.winnie@gmail.com>
2024-10-31 11:48:05 +0800

    rev

---
+++ logics/hdb_query.py
@@ -0,0 +1,87 @@
+from langchain_community.document_loaders import CSVLoader
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from tqdm import tqdm
+import os
+
+load_dotenv()
+
+# Set up embeddings and document loader
+embedding_function = OpenAIEmbeddings(model="text-embedding-3-small")
+loader = CSVLoader("./data/resalehdb.csv", encoding="windows-1252")
+documents = loader.load()
+
+# Specify a directory to persist the database
+persist_directory = "./chroma_db"
+
+# Check if Chroma DB already exists
+if not os.path.exists(persist_directory):
+    print("Creating and persisting Chroma database...")
+    # Generate embeddings with a progress bar and collect them in a list
+    db = Chroma(
+        persist_directory=persist_directory, embedding_function=embedding_function
+    )
+
+    # Iterate through documents with a progress bar
+    for doc in tqdm(documents, desc="Embedding documents"):
+        db.add_documents([doc])  # Add embedding to the Chroma database
+
+    # Persist the database to disk
+    db.persist()
+    print("Chroma database created and persisted.")
+else:
+    # Load the persisted Chroma database
+    print("Loading persisted Chroma database...")
+    db = Chroma(
+        persist_directory=persist_directory, embedding_function=embedding_function
+    )
+
+retriever = db.as_retriever(search_kwargs={"k": 2000})
+print("Chroma database is ready for use.")
+
+
+# Template for the model prompt
+template = """You will be provided with customer service queries about hdb town and resale price.
+
+  Your response must start with Ans:
+Please provide short answer based on:
+{context}
+
+
+Question: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+# Set up the model
+llm = ChatOpenAI(
+    model="gpt-4o-mini", temperature=0, max_tokens=None, timeout=None, max_retries=2
+)
+
+
+# Define the chain
+def query_chain(question):
+    print("Retrieving documents...")
+    # Retrieve relevant documents
+    context_docs = retriever.get_relevant_documents(question)
+    print(context_docs)
+    context = "\n".join(
+        [doc.page_content for doc in context_docs]
+    )  # Combine documents as context
+    formatted_prompt = prompt.format_prompt(context=context, question=question)
+    # Stream the response
+    for chunk in llm.stream(formatted_prompt.to_messages()):
+        yield chunk
+
+
+# Updated this part onwards. Use hdb_json_2 in your main.py file
+def hdb_json_2(query):
+    # Invoke the chain with a sample question
+    for chunk in query_chain(query):
+        print(chunk.content, end="")
+
+
+my_query = "What was the average resale price in Ang Mo Kio for Oct 2024?"
+print(hdb_json_2(my_query))
---
+++ pages/2_main try.py
@@ -0,0 +1,37 @@
+# Set up and run this Streamlit App
+import streamlit as st
+import pandas as pd
+# from helper_functions import llm
+from logics.hdb_query import hdb_json_2
+#from logics.hdb_json_query import hdb_json_2
+from helper_functions.utility import check_password  
+
+# Check if the password is correct.  
+if not check_password():  
+    st.stop()
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="HDB Resale Price Finder"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("HDB Resale Price Finder")
+
+form = st.form(key="form")
+form.subheader("Please ask me about HDB resale data")
+
+user_prompt = form.text_area("Eg. What is the average price of 4 room in Ang Mo Kio town", height=200)
+
+if form.form_submit_button("Submit"):
+    
+    st.toast(f"User Input Submitted - {user_prompt}")
+
+    st.divider()
+
+    hdb_response = hdb_json(user_prompt)
+    st.write(hdb_response)
+
+with st.expander("Disclaimer"):
+    st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
